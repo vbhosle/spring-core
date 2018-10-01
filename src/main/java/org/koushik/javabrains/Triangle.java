18aadd8cf602ef152be6ae71278bbb9ee6dadac0
Viraj Bhosle <bhosleviraj@gmail.com>
2018-10-01 18:10:16 +0530

    Spring Tutorial 10 - Bean Autowiring

--- src/main/java/org/koushik/javabrains/Triangle.java
+++ src/main/java/org/koushik/javabrains/Triangle.java
@@ -1,25 +1,39 @@
 package org.koushik.javabrains;
 
-import java.util.List;
-
 public class Triangle {
 
-	private List<Point> points;
+		private Point pointA;
+		private Point pointB;
+		private Point pointC;
 	
 	public void draw() {
-		System.out.println("Drawing triangle");
-		for(Point point:points) {
-			System.out.println(point);
-		}
+		System.out.println("Triangle drawn from "+ pointA + ", " + pointB +", " + pointC);
 	}
 
-	public List<Point> getPoints() {
-		return points;
+	public Point getPointA() {
+		return pointA;
 	}
 
-	public void setPoints(List<Point> points) {
-		this.points = points;
+	public void setPointA(Point pointA) {
+		this.pointA = pointA;
 	}
 
+	public Point getPointB() {
+		return pointB;
+	}
+
+	public void setPointB(Point pointB) {
+		this.pointB = pointB;
+	}
+
+	public Point getPointC() {
+		return pointC;
+	}
+
+	public void setPointC(Point pointC) {
+		this.pointC = pointC;
+	}
+
+	
 	
 }
