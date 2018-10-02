28e1ec71ac4e4f95e46cb6d36e8ccbd1d80f7c1a
vbhosle <bhosleviraj@gmail.com>
2018-10-02 18:14:26 +0530

    Spring Tutorial 19 - The Autowired Annotation

--- src/main/java/org/koushik/javabrains/Circle.java
+++ src/main/java/org/koushik/javabrains/Circle.java
@@ -1,5 +1,7 @@
 package org.koushik.javabrains;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Required;
 
 public class Circle implements Shape {
@@ -10,7 +12,8 @@ public class Circle implements Shape {
 		return center;
 	}
 
-	@Required
+	@Autowired
+	@Qualifier("pointB")
 	public void setCenter(Point center) {
 		this.center = center;
 	}
